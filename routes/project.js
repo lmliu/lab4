6355308fba788b13abdc2663ff3e7322c577b834
Lauren <lmliu@ucsd.edu>
2016-01-28 17:06:18 +0000

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,8 @@
+exports.viewProject = function(req, res) { 
+  var name = req.params.name; 
+  res.render('project');
+  console.log("The project name is: " + name);
+   res.render('project', {
+    'projectName': name
+  });
+};
